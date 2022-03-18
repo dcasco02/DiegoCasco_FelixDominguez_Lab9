b9f27faccb8ea79596e0fa81ab70bb1f302ebd3a
dcasco02 <dcasco@unitec.edu>
2022-03-18 14:42:12 -0600

    login

--- src/diegocasco_felixdominguez_lab9/DiegoCasco_FelixDominguez_Lab9.java
+++ src/diegocasco_felixdominguez_lab9/DiegoCasco_FelixDominguez_Lab9.java
@@ -15,7 +15,8 @@ public class DiegoCasco_FelixDominguez_Lab9 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        Login l = new Login();
+        l.setVisible(true);
     }
     
 }
